237b7d8507b93967b843fb99688dbf424ad9780f
mcassoli <mcassoli@utexas.edu>
2022-10-08 13:54:36 -0500

    Partially working turtle game!
    Turtle can fire missiles
    It'll quit when you have more than 5 missiles (convenient break trigger. We could add another button or something)
    
    TO DO:
    -need a way to test this via fake serial commands
    -need to figure out turtle.heading. Currently the missiles are oriented properly, but calling turtle.forward makes them go in their current orientation, which is always 0 degrees. I'm not sure why turtle.settiltangle isn't changing their heading, but it seems to just rotate them visually without updating their internal heading.
    
    A bunch of status stuff prints to the python console for now.
    
    I'll poke around more later today, but wanted you to be able to see where I was at for now.

--- basic_arduino_game/basic_game_controller.py
+++ basic_arduino_game/basic_game_controller.py
@@ -7,6 +7,8 @@ Created on Sat Oct  8 10:24:00 2022
 """
 
 import serial
+import turtle
+import time
 
 # ser = serial.Serial('COM3', 9600)
 
@@ -15,3 +17,121 @@ import serial
 
 # ser.readline()
 # Out[6]: b'17697\r\n'
+
+#go get some test input lines
+# ser = serial.Serial('COM3', 9600)
+# lines = []
+# while len(lines) < 10:
+#     lines.append(ser.readline())
+    
+# #parse the lines
+# messages = []
+# for line in lines:
+#     message = ''
+#     decoded = bytes.decode(line)
+    
+#     for char in decoded:
+#         if char != '\n' and char != '\r':
+#             message = message + char
+    
+#     messages.append(message)
+    
+# #see how we did
+# print(messages)
+#   ['108', 'FIRE!', '153', '133', '147', '233', 'FIRE!', '273', '290', 'FIRE!']
+# ser.close()
+
+def test_serial(ser):
+    lines = []
+    while len(lines) < 10:
+        lines.append(ser.readline())
+        
+    #parse the lines
+    messages = []
+    for line in lines:
+        message = ''
+        decoded = bytes.decode(line)
+        
+        for char in decoded:
+            if char != '\n' and char != '\r':
+                message = message + char
+        
+        messages.append(message)
+    
+    return messages
+
+def parse_msg(msg_str):
+    msg_str = bytes.decode(msg_str)
+    msg = ''
+    for char in msg_str:
+        if char != '\n' and char != '\r':
+            msg += char
+    return msg
+
+def shoot(current_angle):
+    '''shoot a projectile
+    '''
+    missile = turtle.Turtle()
+    missile.settiltangle(current_angle)
+    missile.penup()
+    
+    return missile
+
+def draw(msg, missiles, last_heading):
+    '''
+    rotate the player
+    advance any missiles
+    remove any missiles that have gone too far
+    '''
+    print(msg)    
+    
+    if msg != 'FIRE!':
+        last_heading = int(msg)
+        player.settiltangle(last_heading)
+    else: #we're firing a missile!
+        new_missile = shoot(last_heading)
+        missiles.append(new_missile)
+        print('pew!')
+    
+    #advance existing missiles
+    for miss in missiles:
+        miss.forward(MISSILE_SPEED)
+    
+    #remove missiles that are far away:
+    #someone help me with a better way to do this!
+    to_remove = []
+    for idx in range(0, len(missiles)):
+        if missiles[idx].distance(0,0) > MISSILE_DELETE:
+            to_remove.append(idx)
+            
+    to_remove.reverse()
+    for remove_idx in to_remove:
+        del missiles[remove_idx]
+    
+    
+    return missiles, last_heading
+
+#--------------------------------------------------
+MISSILE_DELETE = 220
+MISSILE_SPEED  = 10
+
+ser =serial.Serial('COM3', 9600)
+
+player = turtle.Turtle()
+player.shape('turtle')
+player.color('green')
+missiles = [] #a list of turtle missiles
+window = turtle.Screen()
+last_heading = 0
+
+while(True):
+    missiles, last_heading = draw(parse_msg(ser.readline()), missiles, last_heading)
+    time.sleep(.05)
+    
+    if len(missiles) > 5:
+        print("so many missiles! goodbye")
+        break
+
+
+turtle.bye()
+ser.close()
--- basic_arduino_game/basic_matthew_arduino_game/basic_matthew_arduino_game.ino
+++ basic_arduino_game/basic_matthew_arduino_game/basic_matthew_arduino_game.ino
@@ -1,6 +1,7 @@
 int BUTTON_PIN = 2;
 int SWITCH_PIN = 3;
 int DELAY_TIME = 250;
+int angle = 10;
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,12 +14,13 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (digitalRead(SWITCH_PIN) == HIGH){
     if (digitalRead(BUTTON_PIN) == HIGH){
-      Serial.print('FIRE!');
+      Serial.print("FIRE!");
       Serial.println();
       delay(DELAY_TIME);
     }
     else{
-      Serial.print(analogRead(A0));
+      angle = analogRead(A0);
+      Serial.print(map(angle, 0, 1023, 0, 359));
       Serial.println();
       delay(DELAY_TIME);
     }
