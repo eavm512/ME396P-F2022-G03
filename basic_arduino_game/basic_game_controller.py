49f9f0a20995e00df8eb5e60e7f905ff6598ddcd
mcassoli <mcassoli@utexas.edu>
2022-10-08 11:10:07 -0500

    remove old arduino code, add comments to serial demo

--- basic_arduino_game/basic_game_controller.py
+++ basic_arduino_game/basic_game_controller.py
@@ -8,4 +8,10 @@ Created on Sat Oct  8 10:24:00 2022
 
 import serial
 
-a=5
+# ser = serial.Serial('COM3', 9600)
+
+# ser
+# Out[5]: Serial<id=0x254b51f4c70, open=True>(port='COM3', baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=None, xonxoff=False, rtscts=False, dsrdtr=False)
+
+# ser.readline()
+# Out[6]: b'17697\r\n'
