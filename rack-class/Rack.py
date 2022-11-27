f167295aeb9b138c8bc5195c1e7eafac884fcde7
Kaya-Bayazitoglu <112008945+Kaya-Bayazitoglu@users.noreply.github.com>
2022-11-26 18:44:05 -0600

    Update Rack.py
    "Should" be minimum passing

--- rack-class/Rack.py
+++ rack-class/Rack.py
@@ -1,5 +1,6 @@
-"""
 
+
+"""
 """
 from datetime import datetime
 
@@ -18,18 +19,24 @@ class Rack:
         '''
         self.updated = datetime.now() # set time to now
         
+        # Check each bay in the new status stored in status
         for bay, state in status.items():
+            
+            # If the bay does not already exist, add it and log the update
             if bay not in self.bays:
-                self.bays[bay] = status
-                self.log(bay, state, self.updated)
-            elif state != self.bays[bay]:
-                self.bays = status
-                self.log(bay, state, self.updated)
+                self.bays[bay] = state
+                self.log_bay(bay, state)
+            
+            # If the bay exists and its status has changed, record and log the change
+            elif state is not self.bays[bay]:
+                self.bays[bay] = state
+                self.log_bay(bay, state)
     
-    def log(bay, status, time):
+    def log_bay(self, bay, status):
         '''
         
         '''
         # print the change to a file
         f = open('rack-log', 'a')
-        f.write('')
+        f.write('{0} {1} {2}\n'.format(bay, status, self.updated))
+        f.close()
