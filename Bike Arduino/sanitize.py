34485674d9ceb5f9b2d548ab880eb5cc269a21b7
jmmaweu <112222143+jmmaweu@users.noreply.github.com>
2022-11-29 12:19:54 -0600

    Update sanitize.py

--- Bike Arduino/sanitize.py
+++ Bike Arduino/sanitize.py
@@ -1,15 +1,21 @@
 # sanitize: RX bike rack serial data cooked and pushed to Rack objects
 #   per ME396P-F2022-G03/Standards/board-data-flow.jpeg
+#   per "<'Available '> + <distance> <'\n'>" in Bike Arduino/ESP_BT_Handler.py
+#   per Bike Arduino/BT_with_info_string_ESP32/BT_with_info_string_ESP32.ino
+#   per "Eg_dict = {0 : True, 1: False, 2 : True, 3 : True, 4 : False}" in Standards/Rack_Dict_convention
+#   per Bike Arduino/mod_example_sonar_arduino_uno_version/mod_example_sonar_arduino_uno_version.ino
 
 from ESP_BT_Handler import *
 from Rack import *
 #import Rack as Rack
 import re
 
+line_re = None
+classifier_data = None
 rack = None
 
 def load_classifier_data():
-    pass
+    classifier_data = {'CUTOFF_ONE: 30,'CUTOFF_TWO': 10}
 
 def rack_init(rack_map_data):
     rack = Rack()
@@ -17,13 +23,13 @@ def rack_init(rack_map_data):
 
 def sanitize(rack_map_data, port_baud_list=None):
     def parse_line(line):
-        pass
+        return line_re.match(line)
     
-    def grammar_is_good(ast):
-        pass
+    def grammar_is_good(line_data):
+        return bool(line_data)
    
-    def classify(ast):
-        return {0 : True}
+    def classify(line_data):
+        return {0 : line_data.match(1) == 'AVAILABLE'}
     
     def push_to_rack(the_dict):
         rack.update(the_dict)
@@ -31,15 +37,17 @@ def sanitize(rack_map_data, port_baud_list=None):
     
     def port_listnener(port):
         line = get_messages(port = 7)  ##### udpate port if necessary (unlikely)
-        ast = parse_line(line)
-        if grammar_is_good(ast):
-            the_dict = classify(ast)
+        line_data = parse_line(line)
+        if grammar_is_good(line_data):
+            the_dict = classify(line_data)
             push_to_rack(the_dict)
     
     port_listener()
 
 def supervise():
     load_classifier_data()
+    distance_pat = '(0|[0-9][1-9]*)'
+    line_re = re.compile(r'(AVAILABLE|NOT AVAILABLE) ' + distance_pat + '\n')
     rack_init(rack_map_data)
     while True:
         sanitize()
