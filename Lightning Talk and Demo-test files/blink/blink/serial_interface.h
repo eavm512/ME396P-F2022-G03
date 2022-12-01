ddb8154bd6ca17b56cafe0a7b586403985d955fa
mcassoli <mcassoli@utexas.edu>
2022-11-30 20:27:22 -0600

    start organizing GitHub repo
    move lightning and demo / learning stuff to "Lightning Talk and Demo-test files" folder

---
+++ Lightning Talk and Demo-test files/blink/blink/serial_interface.h
@@ -0,0 +1,3 @@
+ { 'R', "blink_high", "K?", "tell python that we are ready to play" },
+ { 'H', "blink_high", "K?", "tell python that the LED was set high" },
+ { 'L', "blink_low", "K?", "tell python that the LED was set low" },
